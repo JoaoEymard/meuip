87d4b5e11f6a8f38f3bca95383813d434e99aaf3
joaoeymard <eymarduarte12@hotmail.com>
2020-12-21 17:33:03 -0300

    refatorando dockerfile

--- Dockerfile
+++ Dockerfile
@@ -7,14 +7,7 @@ WORKDIR /home/node/app
 RUN rm -rf node_modules
 
 # setup project
-RUN npm install -s
-
-# enviroment
-ENV NODE_ENV=production
-ENV NPM_CONFIG_LOGLEVEL info
-
-# export port to node
-EXPOSE 3000
+RUN npm install --only=prod --quiet
 
 # command
 CMD [ "npm", "start" ]
--- deploy.sh
+++ deploy.sh
@@ -1,13 +1,16 @@
 #!/bin/bash
 
 # variables
-IMAGE="pbr-myip"
-NAME="myipservice"
+NAME="ws-myip"
+IMAGE=$NAME
 VERSION="$(cat VERSION)"
+
+# stop and remove
+docker stop $NAME; docker rm $NAME
 
 # build
 docker build -t $IMAGE:$VERSION .
 
 # run 
-docker run -d -p 3020:3000 \
---name $NAME $IMAGE:$VERSION
+docker run -d -p 3021:3000 \
+  --name $NAME $IMAGE:$VERSION
